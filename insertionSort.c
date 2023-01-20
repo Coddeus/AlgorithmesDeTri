0204fbecf9989322007690f6efb3f5dbfb9a3357
CameliaMallus <codeusbonus@gmail.com>
2023-01-20 01:17:42 +0100

    Filtered most important stuff

--- insertionSort.c
+++ insertionSort.c
@@ -1,32 +1,11 @@
 #include "general.h"
 
-// int toBeSorted[] = {8, 7, 3, 1, 9, 4, 0, 2, 6, 4, 5};
-
-/* int main(){
-    int* toBeSorted = generateRandomList();
-
-    printList(toBeSorted, listLen);
-    for (int i=1;i<listLen;i++){
-        int el=i;
-        while (toBeSorted[el]<toBeSorted[el-1]){
-            int savedEl = toBeSorted[el];
-            toBeSorted[el] = toBeSorted[el-1];
-            toBeSorted[el-1] = savedEl;
-            el--;
-            if (el==0)
-                break;
-        }
-    }
-    printList(toBeSorted, listLen);
-
-    free(toBeSorted);
-    return 0;
-} */
 
 int main(){
     int* toBeSorted = generateRandomList();
 
     printList(toBeSorted, listLen);
+
     for (int i=1;i<listLen;i++){
         int savedEl = toBeSorted[i];
         int el=i;
@@ -38,6 +17,21 @@ int main(){
         }
         toBeSorted[el] = savedEl;
     }
+
+    /*
+    for (int i=1;i<listLen;i++){
+        int el=i;
+        while (toBeSorted[el]<toBeSorted[el-1]){
+            int savedEl = toBeSorted[el];
+            toBeSorted[el] = toBeSorted[el-1];
+            toBeSorted[el-1] = savedEl;
+            el--;
+            if (el==0)
+                break;
+        }
+    }
+    */
+
     printList(toBeSorted, listLen);
 
     free(toBeSorted);
